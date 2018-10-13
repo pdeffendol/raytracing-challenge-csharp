d7c1a068698fabcc1fbc3451bd431bfe21b55c54
Pete Deffendol <pete@deffendol.org>
2018-10-13 09:06:25 -0600

    Add matrix determinant and inverse

--- src/RayTracer.Core/Matrix.cs
+++ src/RayTracer.Core/Matrix.cs
@@ -30,7 +30,7 @@ namespace RayTracer.Core
             var m = new Matrix(size, size);
             for (int i = 0; i < size; i++)
             {
-                m[i,i] = 1;
+                m[i, i] = 1;
             }
 
             return m;
@@ -71,7 +71,7 @@ namespace RayTracer.Core
             {
                 for (var c = 0; c < this.Columns; c++)
                 {
-                    if (!DoubleComparer.Equal(this[r,c], other[r,c]))
+                    if (!DoubleComparer.Equal(this[r, c], other[r, c]))
                     {
                         return false;
                     }
@@ -97,15 +97,15 @@ namespace RayTracer.Core
                     double dp = 0;
                     for (int s = 0; s < m1.Columns; s++)
                     {
-                        dp += m1[r,s] * m2[s,c];
+                        dp += m1[r, s] * m2[s, c];
                     }
-                    product[r,c] = dp;
+                    product[r, c] = dp;
                 }
             }
             return product;
         }
 
-        public static Tuple operator*(Matrix matrix, Tuple tuple)
+        public static Tuple operator *(Matrix matrix, Tuple tuple)
         {
             if (matrix.Columns != 4 || matrix.Rows != 4)
             {
@@ -113,22 +113,22 @@ namespace RayTracer.Core
             }
 
             return new Tuple(
-                tuple.X * matrix[0,0]
-                + tuple.Y * matrix[0,1]
-                + tuple.Z * matrix[0,2]
-                + tuple.W * matrix[0,3],
-                tuple.X * matrix[1,0]
-                + tuple.Y * matrix[1,1]
-                + tuple.Z * matrix[1,2]
-                + tuple.W * matrix[1,3],
-                tuple.X * matrix[2,0]
-                + tuple.Y * matrix[2,1]
-                + tuple.Z * matrix[2,2]
-                + tuple.W * matrix[2,3],
-                tuple.X * matrix[3,0]
-                + tuple.Y * matrix[3,1]
-                + tuple.Z * matrix[3,2]
-                + tuple.W * matrix[3,3]
+                tuple.X * matrix[0, 0]
+                + tuple.Y * matrix[0, 1]
+                + tuple.Z * matrix[0, 2]
+                + tuple.W * matrix[0, 3],
+                tuple.X * matrix[1, 0]
+                + tuple.Y * matrix[1, 1]
+                + tuple.Z * matrix[1, 2]
+                + tuple.W * matrix[1, 3],
+                tuple.X * matrix[2, 0]
+                + tuple.Y * matrix[2, 1]
+                + tuple.Z * matrix[2, 2]
+                + tuple.W * matrix[2, 3],
+                tuple.X * matrix[3, 0]
+                + tuple.Y * matrix[3, 1]
+                + tuple.Z * matrix[3, 2]
+                + tuple.W * matrix[3, 3]
             );
         }
 
@@ -140,11 +140,106 @@ namespace RayTracer.Core
             {
                 for (int c = 0; c < this.Columns; c++)
                 {
-                    m[c,r] = this[r,c];
+                    m[c, r] = this[r, c];
                 }
             }
 
             return m;
+        }
+
+        public double Determinant()
+        {
+            if (this.Rows != this.Columns)
+            {
+                throw new InvalidOperationException("Only square matrices have determinants");
+            }
+
+            if (this.Rows == 2)
+            {
+                return this[0, 0] * this[1, 1] - this[0, 1] * this[1, 0];
+            }
+
+            double det = 0;
+            for (int c = 0; c < Columns; c++)
+            {
+                det += this[0, c] * this.Cofactor(0, c);
+            }
+            return det;
+        }
+
+        public Matrix Submatrix(int rowToRemove, int columnToRemove)
+        {
+            if (rowToRemove < 0 || rowToRemove >= this.Rows)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowToRemove));
+            }
+            if (columnToRemove < 0 || columnToRemove >= this.Columns)
+            {
+                throw new ArgumentOutOfRangeException(nameof(columnToRemove));
+            }
+
+            var m = new Matrix(this.Rows - 1, this.Columns - 1);
+
+            for (int r = 0; r < this.Rows; r++)
+            {
+                int resultRow = r;
+                if (r == rowToRemove)
+                {
+                    continue;
+                }
+                else if (r > rowToRemove)
+                {
+                    resultRow--;
+                }
+                for (int c = 0; c < this.Columns; c++)
+                {
+                    int resultCol = c;
+                    if (c == columnToRemove)
+                    {
+                        continue;
+                    }
+                    else if (c > columnToRemove)
+                    {
+                        resultCol--;
+                    }
+
+                    m[resultRow, resultCol] = this[r, c];
+                }
+            }
+
+            return m;
+        }
+
+        public double Minor(int row, int column)
+        {
+            return this.Submatrix(row, column).Determinant();
+        }
+
+        public double Cofactor(int row, int column)
+        {
+            var factor = (row + column) % 2 == 1 ? -1: 1;
+            return this.Minor(row, column) * factor;
+        }
+
+        public bool IsInvertible()
+        {
+            return this.Determinant() != 0;
+        }
+
+        public Matrix Inverse()
+        {
+            var inverse = new Matrix(Rows, Columns);
+            var determinant = this.Determinant();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    inverse[c, r] = this.Cofactor(r, c) / determinant;
+                }
+            }
+
+            return inverse;
         }
     }
 }
--- test/RayTracer.Core.Tests/MatrixTests.cs
+++ test/RayTracer.Core.Tests/MatrixTests.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using RayTracer.Core;
 using System;
+using System.Collections.Generic;
 
 namespace RayTracer.Core.Tests
 {
@@ -112,7 +113,7 @@ namespace RayTracer.Core.Tests
                 {2, 3, 4, 5},
             });
 
-            Assert.Throws(typeof(InvalidOperationException), () =>
+            Assert.Throws<InvalidOperationException>(() =>
             {
                 var product = m1 * m2;
             });
@@ -201,7 +202,7 @@ namespace RayTracer.Core.Tests
 
             var tuple = new Tuple(1, 2, 3, 1);
 
-            Assert.Throws(typeof(InvalidOperationException), () =>
+            Assert.Throws<InvalidOperationException>(() =>
             {
                 var product = m1 * tuple;
             });
@@ -263,6 +264,214 @@ namespace RayTracer.Core.Tests
             var m = Matrix.Identity(4);
 
             Assert.True(m.Equals(m.Transpose()));
+        }
+
+        [Fact]
+        public void Determinant_2x2()
+        {
+            var m = new Matrix(new double[,] {
+                {1, 5},
+                {-3, 2}
+            });
+
+            var det = m.Determinant();
+
+            Assert.Equal(17, det);
+        }
+
+        [Fact]
+        public void Submatrix_Given3x3_RemovesRowAndColumn()
+        {
+            var m = new Matrix(new double[,] {
+                {1, 5, 0},
+                {-3, 2, 7},
+                {0, 6, -3}
+            });
+
+            var expectedResult = new Matrix(new double[,] {
+                {-3, 2},
+                {0, 6}
+            });
+
+            var result = m.Submatrix(0, 2);
+
+            Assert.True(result.Equals(expectedResult));
+        }
+
+        [Fact]
+        public void Submatrix_Given4x4_RemovesRowAndColumn()
+        {
+            var m = new Matrix(new double[,] {
+                {-6, 1, 1, 6},
+                {-8, 5, 8, 6},
+                {-1, 0, 8, 2},
+                {-7, 1, -1, 1}
+            });
+
+            var expectedResult = new Matrix(new double[,] {
+                {-6, 1, 6},
+                {-8, 8, 6},
+                {-7, -1, 1}
+            });
+
+            var result = m.Submatrix(2, 1);
+
+            Assert.True(result.Equals(expectedResult));
+        }
+
+        [Fact]
+        public void Minor_3x3()
+        {
+            var m = new Matrix(new double[,] {
+                {3 ,5 ,0},
+                {2, -1, -7},
+                {6, -1 ,5}
+            });
+
+            Assert.Equal(25, m.Minor(1, 0));
+        }
+
+        [Fact]
+        public void Cofactor_3x3()
+        {
+            var m = new Matrix(new double[,] {
+                {3 ,5 ,0},
+                {2, -1, -7},
+                {6, -1 ,5}
+            });
+
+            Assert.Equal(-12, m.Cofactor(0, 0));
+            Assert.Equal(-25, m.Cofactor(1, 0));
+        }
+
+        [Fact]
+        public void Determinant_3x3()
+        {
+            var m = new Matrix(new double[,] {
+                {1 ,2 ,6},
+                {-5, 8, -4},
+                {2, 6 ,4}
+            });
+
+            Assert.Equal(56, m.Cofactor(0, 0));
+            Assert.Equal(12, m.Cofactor(0, 1));
+            Assert.Equal(-46, m.Cofactor(0, 2));
+            Assert.Equal(-196, m.Determinant());
+        }
+
+        [Fact]
+        public void Determinant_4x4()
+        {
+            var m = new Matrix(new double[,] {
+                {-2 ,-8 ,3, 5},
+                {-3, 1, 7, 3},
+                {1, 2 ,-9, 6},
+                {-6, 7 ,7, -9}
+            });
+
+            Assert.Equal(690, m.Cofactor(0, 0));
+            Assert.Equal(447, m.Cofactor(0, 1));
+            Assert.Equal(210, m.Cofactor(0, 2));
+            Assert.Equal(51, m.Cofactor(0, 3));
+            Assert.Equal(-4071, m.Determinant());
+        }
+
+        [Fact]
+        public void IsInvertible_GivenNonZeroDeterminant_IsTrue()
+        {
+            var m = new Matrix(new double[,] {
+                {6 ,4 ,4, 4},
+                {5, 5, 7, 6},
+                {4, -9 ,3, -7},
+                {9, 1 ,7, -6}
+            });
+
+            Assert.True(m.IsInvertible());
+        }
+
+        [Fact]
+        public void IsInvertible_GivenZeroDeterminant_IsFalse()
+        {
+            var m = new Matrix(new double[,] {
+                {-4 ,2 ,-2, 3},
+                {9, 6, 2, 6},
+                {0, -5 ,1, -5},
+                {0, 0 ,0, 0}
+            });
+
+            Assert.False(m.IsInvertible());
+        }
+
+        public static IEnumerable<object[]> InverseFixture =>
+            new List<object[]>
+            {
+                new object[] { 
+                    new double[,] {
+                        {-5 ,2 ,6, -8},
+                        {1, -5, 1, 8},
+                        {7, 7 ,-6, -7},
+                        {1, -3 ,7, 4}},
+                    new[,] {
+                        {0.21805, 0.45113, 0.24060, -0.04511},
+                        {-0.80827, -1.45677, -0.44361, 0.52068},
+                        {-0.07895, -0.22368, -0.05263, 0.19737},
+                        {-0.52256, -0.81391, -0.30075, 0.30639}}
+                },
+                new object[] { new double[,] {
+                        {8, -5,  9,  2},
+                        {7,  5,  6,  1},
+                        {-6,  0,  9,  6},
+                        {-3,  0, -9, -4}},
+                   new[,] {
+                        {-0.15385, -0.15385, -0.28205, -0.53846},
+                        {-0.07692,  0.12308,  0.02564,  0.03077},
+                        {0.35897,  0.35897,  0.43590,  0.92308},
+                        {-0.69231, -0.69231, -0.76923, -1.92308}}
+                },
+                    new object[] { new double[,] {
+                        {9, 3, 0, 9},
+                        {-5, -2, -6, -3},
+                        {-4, 9, 6, 4},
+                        {-7, 6, 6, 2}},
+                    new[,] {
+                        {-0.04074, -0.07778, 0.14444, -0.22222},
+                        {-0.07778, 0.03333, 0.36667, -0.33333},
+                        {-0.02901, -0.14630, -0.10926, 0.12963},
+                        { 0.17778, 0.06667, -0.26667, 0.33333}}                
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(InverseFixture))]
+        public void Inverse_4x4(double[,] matrixData, double[,] inverseData)
+        {
+            var m = new Matrix(matrixData);
+
+            var expectedResult = new Matrix(inverseData);
+
+            var inverse = m.Inverse();
+
+            Assert.True(inverse.Equals(expectedResult));
+        }
+
+        [Fact]
+        public void Inverse_MultiplyProductByInverse()
+        {
+            var m1 = new Matrix(new double[,] {
+                {3, -9,  7,  3},
+                {3, -8,  2, -9},
+                {-4,  4,  4,  1},
+                {-6,  5, -1,  1}});
+
+            var m2 = new Matrix(new double[,] {
+                {8, 2, 2, 2},
+                {3, -1, 7, 0},
+                {7, 0, 5, 4},
+                {6, -2, 0, 5}});
+
+            var product = m1 * m2;
+
+            Assert.True(m1.Equals(product * m2.Inverse()));
         }
     }
 }
